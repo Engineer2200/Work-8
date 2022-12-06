4ea9d33f6826b7940ea82b81f4debb7399dee560
Engineer <arsenya.suvorov@mail.ru>
2022-12-06 20:20:48 +0400

    start

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++)
+{
+matrix[i,j] = new Random().Next(0,11);
+Console.Write(matrix[i,j] + " ");
+}
+Console.WriteLine();
+}
+
+Console.Write("Введите строку: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец: ");
+int column = Convert.ToInt32(Console.ReadLine());
+if( line < m && column < n )
+    {
+        Console.WriteLine(matrix[line,column]);
+    }
+else
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
