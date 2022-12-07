04bf2ef2b028f572238f156d3235eefc962180db
Engineer <arsenya.suvorov@mail.ru>
2022-12-07 20:47:49 +0400

    start

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            {
+            matrix[i,j] = new Random().Next(0,11);
+            Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
+double sum = 0;
+double num = 0;
+for(int j = 0 ; j < n; j++)
+    {
+        for(int i = 0 ; i < m; i++)
+            {
+                sum = sum + matrix[i,j];
+               
+            }
+            num = sum/m;
+                Console.Write(Math.Round(num*10)/10 + " ");
+            sum = 0;
+            num = 0;
+
+    }
+
+
